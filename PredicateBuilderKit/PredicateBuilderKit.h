a39d77dd561bd103f1bd766a24bba7b0ecf6fa51
alexandr.sivash <sanny199955@mail.ru>
2023-11-21 13:57:30 +0500

    initial

---
+++ PredicateBuilderKit/PredicateBuilderKit.h
@@ -0,0 +1,18 @@
+//
+//  PredicateBuilderKit.h
+//  PredicateBuilderKit
+//
+//  Created by Alexandr Sivash on 15.11.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PredicateBuilderKit.
+FOUNDATION_EXPORT double PredicateBuilderKitVersionNumber;
+
+//! Project version string for PredicateBuilderKit.
+FOUNDATION_EXPORT const unsigned char PredicateBuilderKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PredicateBuilderKit/PublicHeader.h>
+
+
